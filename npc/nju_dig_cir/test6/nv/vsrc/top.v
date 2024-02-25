ea2937d5196041971cbb786b3ae6caad27c6a775
tracer-ysyx <tracer@ysyx.org>
2024-02-26 00:41:00 +0800

    > sim RTL ysyx_22040000 jjj Linux hehe 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:41:00 up  6:39,  1 user,  load average: 0.72, 0.74, 0.60

--- npc/nju_dig_cir/test6/nv/vsrc/top.v
+++ npc/nju_dig_cir/test6/nv/vsrc/top.v
@@ -1,26 +1,40 @@
 module top (
-    input clk,
-    input rst,
-    input [13:0] sw,
+    input         clk,
+    input         rst,
+    input  [13:0] sw,
+    input         ps2_clk,
+    input         ps2_data,
     output [15:0] ledr,
-    output VGA_CLK,
-    output VGA_HSYNC,
-    output VGA_VSYNC,
-    output VGA_BLANK_N,
-    output [7:0] VGA_R,
-    output [7:0] VGA_G,
-    output [7:0] VGA_B,
-    output [7:0] seg0
+    output        VGA_CLK,
+    output        VGA_HSYNC,
+    output        VGA_VSYNC,
+    output        VGA_BLANK_N,
+    output [ 7:0] VGA_R,
+    output [ 7:0] VGA_G,
+    output [ 7:0] VGA_B,
+    output [ 7:0] seg0
 );
-  ALU alu1 (
-      .op(sw[10:8]),
-      .a(sw[3:0]),
-      .b(sw[7:4]),
-      .clk(clk),
-      .result(ledr[3:0]),
-      .out(ledr[4])
-  );
+  wire       rst_n;
+  wire       load;
+  wire       clk_lfsr;
 
+  reg  [7:0] rand_num;
+  reg  [7:0] seed;
+
+  assign rst_n     = ~rst;
+  assign clk_lfsr  = sw[8];
+  assign ledr[7:0] = rand_num[7:0];
+  assign seed[7:0] = sw[7:0];
+  assign load      = sw[9];
+  
+
+  LFSR lfsr1 (
+      rst_n,
+      clk_lfsr,
+      load,
+      seed,
+      rand_num
+  );
 
   assign VGA_CLK = clk;
 
@@ -29,17 +43,17 @@ module top (
   wire [23:0] vga_data;
 
   vga_ctrl my_vga_ctrl (
-      .pclk(clk),
-      .reset(rst),
+      .pclk    (clk),
+      .reset   (rst),
       .vga_data(vga_data),
-      .h_addr(h_addr),
-      .v_addr(v_addr),
-      .hsync(VGA_HSYNC),
-      .vsync(VGA_VSYNC),
-      .valid(VGA_BLANK_N),
-      .vga_r(VGA_R),
-      .vga_g(VGA_G),
-      .vga_b(VGA_B)
+      .h_addr  (h_addr),
+      .v_addr  (v_addr),
+      .hsync   (VGA_HSYNC),
+      .vsync   (VGA_VSYNC),
+      .valid   (VGA_BLANK_N),
+      .vga_r   (VGA_R),
+      .vga_g   (VGA_G),
+      .vga_b   (VGA_B)
   );
 
 
---
+++ npc/nju_dig_cir/test7/nv/vsrc/top.v
@@ -0,0 +1,141 @@
+module top (
+    input         clk,
+    input         rst,
+    input  [13:0] sw,
+    input         ps2_clk,
+    input         ps2_data,
+    output [15:0] ledr,
+    output        VGA_CLK,
+    output        VGA_HSYNC,
+    output        VGA_VSYNC,
+    output        VGA_BLANK_N,
+    output [ 7:0] VGA_R,
+    output [ 7:0] VGA_G,
+    output [ 7:0] VGA_B,
+    output [ 7:0] seg0,
+    output [ 7:0] seg1,
+    output [ 7:0] seg2,
+    output [ 7:0] seg3,
+    output [ 7:0] seg4
+);
+  wire       clrn;
+  reg  [7:0] data;
+  reg        nextdata_n;
+
+  wire       ready;
+  wire       overflow;
+  reg  [7:0] data_out;
+  reg  [7:0] ascii_out;
+  reg  [3:0] count_ready;
+
+  assign clrn = sw[8];
+
+  ps2_keyboard ps_contr1 (
+      clk,
+      clrn,
+      ps2_clk,
+      ps2_data,
+      data,
+      ready,
+      nextdata_n,
+      overflow
+  );
+
+  hex7seg se1 (
+      data_out[3:0],
+      seg0  //a_g[6:0]->(a,b,c,d,e,f,g)
+  );
+
+  hex7seg se2 (
+      data_out[7:4],
+      seg1  //a_g[6:0]->(a,b,c,d,e,f,g)
+  );
+
+  always @(negedge clk) begin
+    if (ready != 1'b0) begin
+      if (data != 8'hf0) begin
+        data_out = data;
+        $display("receive %x", data_out);
+      end
+      nextdata_n <= 0;
+    end else begin
+      nextdata_n <= 1;
+    end
+  end
+
+
+  always @(negedge clk) begin
+    if (ready != 1'b0) begin
+      if (data == 8'hf0) begin
+        count_ready = count_ready + 1;
+        $display("count %d", count_ready);
+      end
+    end
+  end
+
+  hex7seg se5 (
+      count_ready[3:0],
+      seg4  //a_g[6:0]->(a,b,c,d,e,f,g)
+  );
+
+  ps2_to_ascii ps2ascii (
+      data_out,
+      ascii_out
+  );
+
+  hex7seg se3 (
+      ascii_out[3:0],
+      seg2  //a_g[6:0]->(a,b,c,d,e,f,g)
+  );
+
+  hex7seg se4 (
+      ascii_out[7:4],
+      seg3  //a_g[6:0]->(a,b,c,d,e,f,g)
+  );
+
+
+  assign VGA_CLK = clk;
+
+  wire [ 9:0] h_addr;
+  wire [ 9:0] v_addr;
+  wire [23:0] vga_data;
+
+  vga_ctrl my_vga_ctrl (
+      .pclk    (clk),
+      .reset   (rst),
+      .vga_data(vga_data),
+      .h_addr  (h_addr),
+      .v_addr  (v_addr),
+      .hsync   (VGA_HSYNC),
+      .vsync   (VGA_VSYNC),
+      .valid   (VGA_BLANK_N),
+      .vga_r   (VGA_R),
+      .vga_g   (VGA_G),
+      .vga_b   (VGA_B)
+  );
+
+
+  vmem my_vmem (
+      .h_addr  (h_addr),
+      .v_addr  (v_addr[8:0]),
+      .vga_data(vga_data)
+  );
+
+endmodule
+
+module vmem (
+    input  [ 9:0] h_addr,
+    input  [ 8:0] v_addr,
+    output [23:0] vga_data
+);
+
+  reg [23:0] vga_mem[524287:0];
+
+  initial begin
+    $readmemh("resource/picture.hex", vga_mem);
+  end
+
+  assign vga_data = vga_mem[{h_addr, v_addr}];
+
+endmodule
+
