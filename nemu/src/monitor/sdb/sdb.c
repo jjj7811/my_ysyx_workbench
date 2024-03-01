7ebad02b21fe5b84093ac70dbedac2f325e9d1b3
tracer-ysyx <tracer@ysyx.org>
2024-03-01 14:07:16 +0800

    > compile NEMU ysyx_22040000 jjj Linux hehe 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:07:16 up 39 min,  1 user,  load average: 1.14, 0.72, 0.46

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -44,12 +44,12 @@ static char* rl_gets() {
 
 static int cmd_c(char *args) {
   cpu_exec(-1);
-  return 0;
+  return -1;
 }
 
 
 static int cmd_q(char *args) {
-  return 5;
+  return -1;
 }
 
 static int cmd_help(char *args);
