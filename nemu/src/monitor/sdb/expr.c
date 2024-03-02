ae0ca73d4285a10a24d1d40f04eb96309907a55e
tracer-ysyx <tracer@ysyx.org>
2024-03-03 04:15:25 +0800

    > compile NEMU ysyx_22040000 jjj Linux hehe 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:15:25 up 15:35,  1 user,  load average: 1.09, 1.00, 0.90

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -111,7 +111,7 @@ static bool make_token(char *e) {
 
         switch (rules[i].token_type) {
         case '+':
-          tokens[nr_token++].type = '+';
+          tokens[nr_token].type = '+';
           strncpy(tokens[nr_token++].str, e + position - substr_len,
                   substr_len);
           break;
