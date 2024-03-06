e84c9bac22ba603acb6a15bf9fae4b73067144ba
tracer-ysyx <tracer@ysyx.org>
2024-03-06 21:56:09 +0800

    > compile NEMU ysyx_22040000 jjj Linux hehe 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:56:09 up  6:08,  1 user,  load average: 0.64, 0.32, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -215,14 +215,15 @@ bool check_parentheses(int p, int q) {
   if (tokens[p].type != '(' || tokens[q].type != ')') {
     return false;
   }
-  while (p < q) {
+  while (p <= q) {
     // printf("p:%d q:%d\r\n", p, q);
     if (tokens[p].type == '(') {
       if (tokens[q].type == ')') {
         p++;
         q--;
-        continue;
-      } else {
+      } else if(p==q){
+        return false;
+      }else{
         q--;
       }
       
