bc6026289766f8323855f2542f05329661ecdfe6
tracer-ysyx <tracer@ysyx.org>
2024-03-23 22:03:38 +0800

    > compile NEMU ysyx_22040000 jjj Linux hehe 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:03:38 up  1:34,  1 user,  load average: 0.71, 0.36, 0.37

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -25,9 +25,10 @@ enum {
   TK_EQ = 1,
   TK_NEQ = 2,
   TK_AND = 3,
+  TK_OR = 4,
   // L_par = 2,
   // R_par = 3,
-  NUM = 4,
+  NUM = 5,
 
   /* TODO: Add more token types */
 
@@ -55,6 +56,7 @@ static struct rule {
     {"==", TK_EQ},  // equal
     {"!=", TK_NEQ}, // no equal
     {"&&", TK_AND}, //&&
+    {"\\|\\|", TK_OR},	
 
 
     {"[0-9]*", NUM}, // number
@@ -165,11 +167,11 @@ static bool make_token(char *e) {
           strncpy(tokens[nr_token++].str, e + position - substr_len,
                   substr_len);
           break;
-        // case TK_OR:
-        //   tokens[nr_token].type = TK_OR;
-        //   strncpy(tokens[nr_token++].str, e + position - substr_len,
-        //           substr_len);
-        //   break;
+        case TK_OR:
+          tokens[nr_token].type = TK_OR;
+          strncpy(tokens[nr_token++].str, e + position - substr_len,
+                  substr_len);
+          break;
         default:
           // TODO();
         }
@@ -203,8 +205,8 @@ int op_pri(int op_type) {
     return 7;
   case TK_AND:
     return 11;
-  // case TK_OR:
-  //   return 12;
+  case TK_OR:
+    return 12;
   default:
     printf("Undefine oprator\n");
     printf("type:%d\r\n", op_type);
@@ -347,9 +349,9 @@ u_int32_t eval(int p, int q, bool *success) {
     case TK_AND:
       return val1 && val2;
       break;
-    // case TK_OR:
-    //   return val1 || val2;
-    //   break;
+    case TK_OR:
+      return val1 || val2;
+      break;
     default:
       printf("wrong op\r\n");
       break;
