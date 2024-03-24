f8a57d672f4da74115758e7d8f90c3056145b1ea
tracer-ysyx <tracer@ysyx.org>
2024-03-24 17:32:34 +0800

    > compile NEMU ysyx_22040000 jjj Linux hehe 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:32:34 up 13 min,  1 user,  load average: 0.04, 0.21, 0.22

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -60,7 +60,7 @@ static struct rule {
     {"\\|\\|", TK_OR},
 
     {"[0-9]*", NUM}, // number
-    {"\\$[a-zA-Z][a-zA-Z]+", REG},
+    {"\\$[a-zA-Z]*[0-9]", REG},
 };
 
 #define NR_REGEX ARRLEN(rules)
