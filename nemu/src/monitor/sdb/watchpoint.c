e042e181df55d22779eb36e09d73697e1697d8ab
tracer-ysyx <tracer@ysyx.org>
2024-03-31 20:34:54 +0800

    > compile NEMU ysyx_22040000 jjj Linux hehe 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:34:54 up 13 min,  1 user,  load average: 2.13, 1.26, 0.81

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -1,21 +1,21 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include "sdb.h"
 
-#define NR_WP 32
+#define NR_WP 2
 
 typedef struct watchpoint {
   int NO;
@@ -29,10 +29,10 @@ typedef struct watchpoint {
 
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
-
+// static int wp_cnts = 0;
 void init_wp_pool() {
   int i;
-  for (i = 0; i < NR_WP; i ++) {
+  for (i = 0; i < NR_WP; i++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
   }
@@ -42,15 +42,22 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
-//free_链表里面存储NR_WP个监视点池，执行该函数将从池中取出一个监视点并返回。
-WP* new_wp(){
-  WP *p = free_;
+// free_链表里面存储NR_WP个监视点池，执行该函数将从池中取出一个监视点并返回。
+WP *new_wp() {
+  if(free_->next != NULL){
+    WP *p = free_;
   free_ = free_->next;
   return p;
+  }else{
+    printf("监视点已用完\r\n");
+    assert(0);
+  }
+  
+  
 }
 
-//调用new_wp取出一个监视点，将监视点插入head链表中
-bool set_watchpoint(char *args){
+// 调用new_wp取出一个监视点，将监视点插入head链表中
+bool set_watchpoint(char *args) {
   WP *p = new_wp();
 
   p->next = head;
