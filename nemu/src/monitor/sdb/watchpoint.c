e32a2700651f12e3bd03fb8c6aaec3b66e13d3ec
tracer-ysyx <tracer@ysyx.org>
2024-04-04 19:39:36 +0800

    > compile NEMU ysyx_22040000 jjj Linux hehe 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:39:36 up  1:10,  1 user,  load average: 0.49, 0.69, 0.57

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -139,7 +139,7 @@ void disp_info_w() {
   WP *p;
   p = head;
   while (p != NULL) {
-    printf("NO:%d\texpr:%s\t,value_now:%d\r\n",p->NO,p->expr,p->value);
+    printf("NO:%2d\texpr:%20s\t,value_now:%5d\r\n",p->NO,p->expr,p->value);
     p = p->next;
   }
 }
